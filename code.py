721043ca7dd89618e7d235f21890bd7650ef670d
mzaeemnasir <m.zaeem.nasir@gmail.com>
2022-07-06 18:32:24 +0500

    Added Picture

--- code.py
+++ code.py
@@ -1,16 +1,28 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw  # Importing the necessary modules
+from config import *  # This file Contains the API Keys and other sensitive information
 
 
-my_image = Image.open("nature.jpeg")
+my_image = Image.open("btc.png")
+# applying Text in the Middle of the Image
+title_font = ImageFont.truetype("./Montserrat-ExtraBold.ttf", 220, encoding="unic")
 
-title_font = ImageFont.truetype("./K2D-Regular.ttf", 200)
 
-
-title_text = "The Beauty of Nature"
+title_text = "$69,6969"
 
 image_editable = ImageDraw.Draw(my_image)
 
+width, height = my_image.size
 
-image_editable.text((15, 15), title_text, (0, 0, 0), font=title_font)
 
-my_image.save("result.jpeg")
+width, height = my_image.size
+
+textW, textH = image_editable.textsize(title_text, font=title_font)
+image_editable.text(
+    ((width - textW) / 2, ((height - textH) / 2) - 30),
+    title_text,
+    (13, 8, 93),
+    font=title_font,
+)
+
+
+my_image.save("result.png")
